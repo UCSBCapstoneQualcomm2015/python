133189e3b6505fe55e682e1f2ce821efdcb6c377
nadoora <nader@umail.ucsb.edu>
2015-11-12 23:55:56 -0800

    updated class, updated readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # python
 python scripts (mostly for algorithm)
+
+
+rfid_class.py 			: class specification file
+rfid_class.tb 			: format for testbenches
+rfid_class_testbench.py : reads in ^^, then runs algorithm
+test_alg.py             : driver for algorithm.py
+algorithm.py            : the money :)
--- rfid_class.py
+++ rfid_class.py
@@ -10,20 +10,76 @@
 ##          RFID TAG CLASS           ##
 ##                                   ##
 #######################################
+import datetime
+
 class rfid_tags:
 	
 #######################################
 ##            constructor            ##
 #######################################
-	def __init__(self, id, dA, dB, dC, dD, lM):
+	def __init__(self, id):
+		
+		self._ID = id ##should be const
+
+		##all vars init to 0, use init function to initialize properly, or setters 
+		self._dist_A = -1
+		self._dist_B = -1
+		self._dist_C = -1
+		self._dist_D = -1
+		self._lastMoved = -1
+
+#######################################
+##            initialize             ##
+##     so constructor can be empty   ##
+#######################################
+	def initialize(self, dA, dB, dC, dDa ):
 		self._dist_A = dA
 		self._dist_B = dB
 		self._dist_C = dC
 		self._dist_D = dD
-		self._proximity_x = 0
-		self._proximity_y = 0
-		self._ID = id
-		self._lastMoved = lM
+		## if its being set, its the last time it was moved
+		self._lastMoved = datetime.datetime.now()
+
+
+#######################################
+##   GETTERS FOR SNIFFER DISTANCES    #
+#######################################
+
+	def getDist_A(self):
+		## [TODO]: write the location algorithm here
+		return self._dist_A
+
+	def getDist_B(self):
+		## [TODO]: write the location algorithm here
+		return self._dist_B
+
+	def getDist_C(self):
+		## [TODO]: write the location algorithm here
+		return self._dist_C
+
+	def getDist_D(self):
+		## [TODO]: write the location algorithm here
+		return self._dist_D
+
+#######################################
+##   SETTERS FOR SNIFFER DISTANCES    #
+#######################################
+
+	def setDist_A(self, dA):
+		## [TODO]: write the location algorithm here
+		self._dist_A = dA
+
+	def setDist_B(self, dB):
+		## [TODO]: write the location algorithm here
+		self._dist_B = dB
+
+	def setDist_C(self, dC):
+		## [TODO]: write the location algorithm here
+		self._dist_C = dC
+
+	def setDist_D(self, dD):
+		## [TODO]: write the location algorithm here
+		self._dist_D = dD
 
 
 #######################################
@@ -33,30 +89,4 @@ class rfid_tags:
 		## [TODO]: write the location algorithm here
 		return self._ID
 
-#######################################
-##        location algorithm         ##
-#######################################
-	def locate(self):
-		## [TODO]: write the location algorithm here
-		pass
-
-
 ##end_class
-
-
-
-
-
-
-
-
-##Testing it works as expected
-# x = rfid_tags(1,2,3,4, '7344245', '0000000')
-# print x._dist_A
-# print x._dist_B
-# print x._dist_C
-# print x._dist_D
-# print x._proximity_y
-# print x._proximity_x
-# print x._ID
-# print x._lastMoved

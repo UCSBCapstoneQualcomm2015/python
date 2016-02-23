969ec0b9b8c5d19796698fcd226e7b2f94b5c26f
sososohan <sohan32094@yahoo.com>
2016-02-22 20:47:58 -0800

    algorithm should work

--- parse_to_json.py
+++ parse_to_json.py
@@ -11,9 +11,11 @@ from rfid_class import *
 sample1 = '{"snaps": [{"snap1" : [{"ids": ["a", "b"]}, {"sig_strength" : ["1", "2"]}]}]}'
 sample2 = '{"cars": {"Nissan": [{"model":"Sentra", "doors":4},{"model":"Maxima", "doors":4}],"Ford": [{"model":"Taurus", "doors":4},{"model":"Escort", "doors":4}]}}'
 sample3 = '{"snaps": [{"name": "snap1", "ids":["1", "4", "3","2"], "sig_strength":[23, 25, 24, 28]}, {"name": "snap2", "ids":["2", "1", "0"], "sig_strength":[21, 28,15]}, {"name": "snap3", "ids":["5", "2", "3","4"], "sig_strength":[21, 29,23,22]}]}'
-sample4 = '{"snaps": [{"ids":[1, 4, 5, 2], "sig_strength":[23, 25, 24, 28]}]}'
+sample4 = '{"snaps": [{"sig_strength": [27.867514221455615, 29.694119124784834, 24.082399653118497, 26.702458530741243, 27.3559889969818, 26.020599913279625, 27.172844520170994], "ids": ["230000000000", "310000000000", "220000000000", "430000000000", "320000000000", "410000000000", "520000000000"]},{"sig_strength": [30.159061356651854, 28.597385661971472, 29.405164849325672, 30.637959859789461, 32.263420871636306, 25.514499979728754, 28.325089127062363, 26.020599913279625, 25.31478917042255], "ids": ["230000000000", "310000000000", "220000000000", "430000000000", "320000000000", "510000000000", "410000000000", "520000000000", "110000000000"]},{"sig_strength": [22.04119982655925, 28.061799739838872, 30.253058652647702, 24.653828514484182, 25.31478917042255], "ids": ["220000000000", "520000000000", "510000000000", "430000000000", "410000000000"]},{"sig_strength": [26.020599913279625, 25.334237554869496, 21.871908490254413, 22.92256071356476, 25.31478917042255, 31.335389083702175], "ids": ["230000000000", "120000000000", "220000000000", "430000000000", "320000000000", "110000000000"]}]}' 
 
-
+vic_sample1 = '{"snaps": [{"sig_strength": [30.810311590506647, 27.3559889969818, 29.151891030953777, 30.58945731389656, 32.263420871636306, 25.211696022817751, 28.964982392847119, 25.051499783199059], "ids": ["230000000000", "310000000000", "220000000000", "430000000000", "320000000000", "520000000000", "410000000000", "110000000000"]},{"sig_strength": [26.848453616444125, 29.912260756924951, 24.149733479708178, 27.456248016875179, 26.855708892230908, 27.3559889969818, 23.010299956639813], "ids": ["230000000000", "310000000000", "220000000000", "430000000000", "320000000000", "520000000000", "110000000000"]},{"sig_strength": [26.989700043360187, 25.282999881463908, 30.644579892269185, 25.158738437116792], "ids": ["230000000000", "120000000000", "110000000000", "320000000000"]},{"sig_strength": [28.061799739838872, 29.700589333738591, 23.332589902774405, 25.888317255942074], "ids": ["520000000000", "510000000000", "310000000000", "410000000000"]}]}'
+vic_id =  '230000000000'
+vic_tags = '{"tags" : ["120000000000","220000000000","320000000000","310000000000","410000000000","430000000000","510000000000","520000000000"]}'
 
 # failed to launch algorithm 1. snapdragon doesn't respond 2. 
 
@@ -54,9 +56,9 @@ def populate_tag(tag, jsonString):
 		currentSnap = jsonString['snaps'][i]
 		if (currentSnap['ids'].count(tag.getID()) == 1): 		#SNAPDRAGON LOCATED THIS TAG
 			tag.snaps[i] = currentSnap['sig_strength'][currentSnap['ids'].index(tag.getID())]
-	print tag.getID()
-	print tag.getDistances()
-	print " "
+	# print tag.getID()
+	# print tag.getDistances()
+	# print " "
 	return tag
 
 
@@ -70,6 +72,7 @@ def nClosest(nSniffers, rfidList, missingID): ##make nSnif = 4 as default
 
 	missingDist = missingID.getDistances()
 	sniffer_proximity_lists = defaultdict()
+
 
 
 	for i in range( 0, nSniffers ): ##each sniffer gets a list of rfid tags
@@ -94,11 +97,11 @@ def nClosest(nSniffers, rfidList, missingID): ##make nSnif = 4 as default
 
 def compareLists(list1, list2, matches):
 ## NOTE: whatever calls this, must take the returned dictionary and append its changes to the main one.
-	# for i in range(0, len(list1)):
-	# 	print list1[i].getID()
-	# print " "
-	# for i in range(0, len(list2)):
-	# 	print list2[i].getID()
+	for i in range(0, len(list1)):
+		print list1[i].getID()
+	print " "
+	for i in range(0, len(list2)):
+		print list2[i].getID()
 	if (len(list1) != len(list2)):
 		raise ValueError('The two lists have different lengths.')
 
@@ -154,19 +157,19 @@ def oneSnapDragon(rfidList, missingID):
 if __name__=="__main__":
 	itemId = sys.argv[2]
 	snapdragons = delete_blanks(sys.argv[1], itemId)
-	refTags = yaml.load(sys.argv[3])['tags']
-	# snapdragons = delete_blanks('{"snaps": [{"sig_strength": [25, 26, 21, 24, 23, 23, 31], "ids": ["230000000000", "120000000000", "220000000000", "430000000000", "320000000000", "510000000000", "110000000000"]},{"sig_strength": [26, 29, 24, 27, 28, 25, 26], "ids": ["230000000000", "310000000000", "220000000000", "430000000000", "320000000000", "410000000000", "520000000000"]}]}', "230000000000")
-	# print(snapdragons['snaps'])
-	# print len(snapdragons['snaps'])
+	refTags = yaml.load(sys.argv[3])['tags']	
+
+	# snapdragons = delete_blanks(vic_sample1, vic_id)
 	# del snapdragons['snaps'][0]
 	# print len(snapdragons['snaps'])
 	tags = initialize_tag_objects(refTags, len(snapdragons['snaps']))
 	# tags = initialize_tag_objects(["1","2","3","5"], len(snapdragons['snaps']))
 
-	item = initialize_item(itemId, len(snapdragons['snaps']))
+	item = initialize_item('230000000000', len(snapdragons['snaps']))
 
 
 	item = populate_tag(item, snapdragons)
+
 
 	for tag in tags:
 		tag = populate_tag(tag, snapdragons)
@@ -179,7 +182,8 @@ if __name__=="__main__":
 			if len(closestResults[i]) < int ( len(tags) ** (.5)):
 				del closestResults[i]
 		for i in range(0, len(closestResults) - 1):
-			matches = compareLists(closestResults[i],closestResults[i + 1], matches)
+			for j in range(1, len(closestResults)):
+				matches = compareLists(closestResults[i],closestResults[j], matches)
 
 	elif (len(snapdragons['snaps']) == 1):
 		matches = oneSnapDragon(tags, item)
